14fedc63dd8b2f42781eea9f5f8244d2a5b51301
Miles Carlsten <carlsten@princeton.edu>
2015-01-09 13:58:27 -0500

    adding bitcoinj code

--- java-socks-proxy-server/src/org/princeton/btcsocks/server/SocksProxySelector.java
+++ java-socks-proxy-server/src/org/princeton/btcsocks/server/SocksProxySelector.java
@@ -1,20 +1,50 @@
 package org.princeton.btcsocks.server;
+//package org.bitcoinj.examples;
+
+import org.bitcoinj.core.*;
+import org.bitcoinj.examples.ExamplePaymentChannelClient;
+import org.bitcoinj.kits.WalletAppKit;
+import org.bitcoinj.params.RegTestParams;
+import org.bitcoinj.protocols.channels.PaymentChannelClientConnection;
+import org.bitcoinj.protocols.channels.StoredPaymentChannelClientStates;
+import org.bitcoinj.protocols.channels.ValueOutOfRangeException;
+import org.bitcoinj.utils.BriefLogFormatter;
+import org.bitcoinj.utils.Threading;
+import com.google.common.collect.ImmutableList;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.Uninterruptibles;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+
+import static org.bitcoinj.core.Coin.CENT;
 
 import java.net.*;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.io.IOException;
 
 import org.princeton.btsocks.discovery.BittorrentDiscovery;
 import org.princeton.btsocks.discovery.RemoteProxyAddress;
 
-import com.google.common.net.InetAddresses;
-
 // Taken from https://docs.oracle.com/javase/8/docs/technotes/guides/net/proxies.html
 
 public class SocksProxySelector extends ProxySelector {
+
+	private static final org.slf4j.Logger log = LoggerFactory.getLogger(ExamplePaymentChannelClient.class);
+	private WalletAppKit appKit;
+	private final Coin channelSize;
+	private final ECKey myKey;
+	private final NetworkParameters params;
+
+
 	// Keep a reference on the previous default
 	ProxySelector defsel = null;
 	private int requestNum = 0;
@@ -31,16 +61,49 @@ public class SocksProxySelector extends ProxySelector {
 
 		// Populate the HashMap (List of proxies)
 		List<RemoteProxyAddress> proxyList = getActiveProxies();
+		System.out.println("Active Proxy List:\n" + proxyList);
 		for (RemoteProxyAddress proxyAddress : proxyList) {
 			proxies.put(proxyAddress.address(), proxyAddress);
 			System.out.println(proxyAddress);
 		}
+
+
+		BriefLogFormatter.init();
+		channelSize = CENT;
+		myKey = new ECKey();
+		params = RegTestParams.get();
+
+		appKit = new WalletAppKit(params, new File("."), "payment_channel_example_client") {
+			@Override
+			protected List<WalletExtension> provideWalletExtensions() {
+				// The StoredPaymentChannelClientStates object is responsible for, amongst other things, broadcasting
+				// the refund transaction if its lock time has expired. It also persists channels so we can resume them
+				// after a restart.
+				// We should not send a PeerGroup in the StoredPaymentChannelClientStates constructor
+				// since WalletAppKit will find it for us.
+				return ImmutableList.<WalletExtension>of(new StoredPaymentChannelClientStates(null));
+			}
+		};
+
+		appKit.connectToLocalHost();
+		appKit.startAsync();
+		appKit.awaitRunning();
+		// We now have active network connections and a fully synced wallet.
+		// Add a new key which will be used for the multisig contract.
+		appKit.wallet().importKey(myKey); //TODO fix: i think this makes it generate a new key every time the program is run
+		appKit.wallet().allowSpendingUnconfirmedTransactions();
+
+		System.out.println(appKit.wallet());
+
+		waitForSufficientBalance(channelSize);
+
 	}
 	
 	public List<RemoteProxyAddress> getActiveProxies() {
 		try {
 			BittorrentDiscovery discovery = new BittorrentDiscovery(InetAddress.getLocalHost(), 6969);
 			List<RemoteProxyAddress> proxyList = discovery.getProxies();
+			System.out.println(proxyList);
 			return proxyList;
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
@@ -86,6 +149,29 @@ public class SocksProxySelector extends ProxySelector {
 				}
 				i++;
 			}
+
+			final String host = "127.0.0.1";//TODO Fix this-- should be defined based on the address of the proxy
+			final int timeoutSecs = 15;
+			final InetSocketAddress server = new InetSocketAddress(host, 4242);
+
+			log.info("Round one ...");
+			System.out.println("Before Payment:");
+			System.out.println(appKit.wallet());
+			try {
+				openAndSend(timeoutSecs, server, host, 9);
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (ValueOutOfRangeException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			log.info(appKit.wallet().toString());
+			log.info("Stopping ...");
+			System.out.println("After Payment:");
+			System.out.println(appKit.wallet());
+			appKit.stopAsync();
+			appKit.awaitTerminated();
 			return l;
 		}
 
@@ -131,6 +217,74 @@ public class SocksProxySelector extends ProxySelector {
 			 */
 			if (defsel != null)
 				defsel.connectFailed(uri, sa, ioe);
+		}
+	}
+	private void openAndSend(int timeoutSecs, InetSocketAddress server, String channelID, final int times) throws IOException, ValueOutOfRangeException, InterruptedException {
+		PaymentChannelClientConnection client = new PaymentChannelClientConnection(
+				server, timeoutSecs, appKit.wallet(), myKey, channelSize.add(Wallet.SendRequest.DEFAULT_FEE_PER_KB), channelID);
+		System.out.println("Putting " + channelSize.add(Wallet.SendRequest.DEFAULT_FEE_PER_KB) + " in channel");
+		// Opening the channel requires talking to the server, so it's asynchronous.
+		final CountDownLatch latch = new CountDownLatch(1);
+		Futures.addCallback(client.getChannelOpenFuture(), new FutureCallback<PaymentChannelClientConnection>() {
+			@Override
+			public void onSuccess(PaymentChannelClientConnection client) {
+				// By the time we get here, if the channel is new then we already made a micropayment! The reason is,
+				// we are not allowed to have payment channels that pay nothing at all.
+				log.info("Success! Trying to make {} micropayments. Already paid {} satoshis on this channel",
+						times, client.state().getValueSpent());
+				final Coin MICROPAYMENT_SIZE = CENT.divide(10);
+				for (int i = 0; i < times; i++) {
+					System.out.println("round:"+ i+ " of transactions");
+					try {
+						// Wait because the act of making a micropayment is async, and we're not allowed to overlap.
+						// This callback is running on the user thread (see the last lines in openAndSend) so it's safe
+						// for us to block here: if we didn't select the right thread, we'd end up blocking the payment
+						// channels thread and would deadlock.
+						Uninterruptibles.getUninterruptibly(client.incrementPayment(MICROPAYMENT_SIZE));
+					} catch (ValueOutOfRangeException e) {
+						log.error("Failed to increment payment by a CENT, remaining value is {}", client.state().getValueRefunded());
+						throw new RuntimeException(e);
+					} catch (ExecutionException e) {
+						log.error("Failed to increment payment", e);
+						throw new RuntimeException(e);
+					}
+					log.info("Successfully sent payment of one CENT, total remaining on channel is now {}", client.state().getValueRefunded());
+				}
+				// if (client.state().getValueRefunded().compareTo(MICROPAYMENT_SIZE) < 0) {
+				if (true) {
+					// Now tell the server we're done so they should broadcast the final transaction and refund us what's
+					// left. If we never do this then eventually the server will time out and do it anyway and if the
+					// server goes away for longer, then eventually WE will time out and the refund tx will get broadcast
+					// by ourselves.
+					System.out.println("settling channel and closing");
+					log.info("Settling channel for good");
+					client.settle();
+				} else {
+					// Just unplug from the server but leave the channel open so it can resume later.
+					System.out.println("close w/o settling. Not clean");
+					client.disconnectWithoutSettlement();
+				}
+				latch.countDown();
+			}
+
+			@Override
+			public void onFailure(Throwable throwable) {
+				log.error("Failed to open connection", throwable);
+				latch.countDown();
+			}
+		}, Threading.USER_THREAD);
+		latch.await();
+	}
+
+	private void waitForSufficientBalance(Coin amount) {
+		// Not enough money in the wallet.
+		Coin amountPlusFee = amount.add(Wallet.SendRequest.DEFAULT_FEE_PER_KB);
+		// ESTIMATED because we don't really need to wait for confirmation.
+		ListenableFuture<Coin> balanceFuture = appKit.wallet().getBalanceFuture(amountPlusFee, Wallet.BalanceType.ESTIMATED);
+		if (!balanceFuture.isDone()) {
+			System.out.println("Please send " + amountPlusFee.toFriendlyString() +
+					" to " + myKey.toAddress(params));
+			Futures.getUnchecked(balanceFuture);
 		}
 	}
 }
